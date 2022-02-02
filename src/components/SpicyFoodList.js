bd32e4cfae6cf852856ab62291fb851dfd4bb4d9
Nicole Candiotti <nccandiotti@gmail.com>
2022-02-02 11:59:27 -0500

    commit tree 10235655da56d32b82de69442a7741f0251f7d79

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
-import { spicyFoods, getNewSpicyFood } from "../data";
+import React, { useState } from "react"
+import { spicyFoods, getNewSpicyFood } from "../data"
 
 function SpicyFoodList() {
-  const [foods, setFoods] = useState(spicyFoods);
+  const [foods, setFoods] = useState(spicyFoods)
 
   function handleAddFood() {
-    const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newFood = getNewSpicyFood()
+    setFoods((foods) => [...foods, newFood])
+    console.log(newFood)
   }
 
   const foodList = foods.map((food) => (
     <li key={food.id}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
-  ));
+  ))
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
-  );
+  )
 }
 
-export default SpicyFoodList;
+export default SpicyFoodList
